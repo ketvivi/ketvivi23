1f47062fb50e886da823330980b5374c869af236
ketvivi <ketlyvitoria27@gmail.com>
2024-11-26 11:25:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,90 @@
+criaCartao(
+    'FUTEBOL',
+    'quem foi o campeão da Copa do Mundo de 2006 ?',
+    'a Seleção campeã da Copa de 2006 foi a Italia '
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual time foi campeão da libertadores de 2012 ?',
+    'o campeão da libertadores de 2012 foi o CORINTHIANS, que logo em seguida foi campeão Mundial de Clubes!'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quem é o maior campeão da libertadores ?',
+    'O maior campeão da libertadores é o Independiente da Argentina com 7 Titulos '
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual pais tem mais titulos de libertadores ?',
+    'A Argentina tem 25 titulos ! o Brasil vem logo em seguida com 23 titulos'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual Seleção venceu a Copa do Mundo de 1998 ?',
+    'A equipe vencedora da Copa do Mundo de 1998 foi a Seleção da França!'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual Seleção venceu a Copa do Mundo de 1970 ?',
+    'A equipe vencedora da Copa do Mundo de 1970 foi a Seleção do Brasil !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual Seleção venceu a Copa do Mundo de 1954 ?',
+    'A equipe vencedora da Copa do Mundo de 1954 foi a Seleção da Alemanha !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual Seleção venceu a Copa do Mundo de 1966?',
+    'A equipe vencedora da Copa do Mundo de 1966 foi a Seleção da Inglaterra !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Qual Seleção venceu a Copa do Mundo de 1930?',
+    'A equipe vencedora da Copa do Mundo de 1930 foi a Seleção do Uruguai !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quantos titulos do campeonato Brasileiro o Corinthians tem ?',
+    'A equipe do Corinthians tem 7 Titulos do campeonato Brasileiro ( 1990. 1998, 1999,2005,2011, 2015, 2017!'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quantos titulos do campeonato Brasileiro o Cruzeiro tem ?',
+    'A equipe do Cruzeiro tem 4 Titulos do campeonato Brasileiro ( 1966,2003,2013, 2014) !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quantos titulos do campeonato Brasileiro o São Paulo tem ?',
+    'A equipe do São Paulo tem 6 Titulos do campeonato Brasileiro ( 1977,1986, 1991,2006,2007,2008) !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quantos titulos do campeonato Brasileiro o Palmeiras tem ?',
+    'A equipe do Palmeiras tem 12 Titulos do campeonato Brasileiro( 1960,1967,1969,1972, 1973, 1993, 1994, 2016,2018, 2022) !'
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quantos titulos do campeonato Brasileiro o Santos tem ?',
+    'A equipe do Santos tem 8 Titulos do campeonato Brasileiro ( 1961, 1962, 1963, 1964, 1965,1968,2002,2004) !'
+
+)
+
+criaCartao(
+    'FUTEBOL',
+    'Quantos titulos do campeonato Brasileiro o Flamengo tem ?',
+    'A equipe do Flamengo  tem 8 Titulos do campeonato Brasileiro ( 1980,1982, 1983,1987, 1992, 2009, 2019,2020) !'
+)
